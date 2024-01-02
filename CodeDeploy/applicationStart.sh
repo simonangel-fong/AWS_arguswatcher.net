acf4425c5954fca6ab4b738ea596ca717cc6d8eb
simonangel-fong <simonangelfong@gmail.com>
2024-01-02 00:10:15 -0500

    test for codepipeline04

--- CodeDeploy/applicationStart.sh
+++ CodeDeploy/applicationStart.sh
@@ -14,21 +14,29 @@ P_GITHUB_REPO_NAME=AWS_arguswatcher_net # github repo name
 P_PROJECT_NAME=Arguswatcher             # the name of django project name
 
 P_HOME=/home/ubuntu                             # path of home dir
+P_LOG=${P_HOME}/log                             # log file
 P_VENV_PATH=${P_HOME}/env                       # path of venv
 P_REPO_PATH=${P_HOME}/${P_GITHUB_REPO_NAME}     # path of repo
 P_PROJECT_PATH=${P_REPO_PATH}/${P_PROJECT_NAME} # path of project, where the manage.py locates.
 
+log() {
+    sudo echo -e "$(date +'%Y-%m-%d %R'): ${1}" >>$P_LOG
+}
+
 ###########################################################
 ## Django migrations
 ###########################################################
-source ${P_VENV_PATH}/bin/activate # activate venv
-echo -e "$(date +'%Y-%m-%d %R') activate venv" >>~/log
+# activate venv
+source ${P_VENV_PATH}/bin/activate &&
+    log "activate venv" || log "Fail: activate venv"
 
-python3 ${P_PROJECT_PATH}/manage.py makemigrations # update changes
-echo -e "$(date +'%Y-%m-%d %R') update changes" >>~/log
+# update changes
+python3 ${P_PROJECT_PATH}/manage.py makemigrations &&
+    log "update changes" || log "Fail: update changes"
 
-python3 ${P_PROJECT_PATH}/manage.py migrate # migrate changes
-echo -e "$(date +'%Y-%m-%d %R') migrate changes" >>~/log
+# migrate changes
+python3 ${P_PROJECT_PATH}/manage.py migrate &&
+    log "migrate changes" || log "Fail: migrate changes"
 
 # python3 manage.py collectstatic
 # python3 ${P_PROJECT_PATH}/manage.py collectstatic # update changes
@@ -36,8 +44,8 @@ echo -e "$(date +'%Y-%m-%d %R') migrate changes" >>~/log
 ###########################################################
 ## restart gunicorn + restart
 ###########################################################
-sudo service gunicorn restart
-echo -e "$(date +'%Y-%m-%d %R') restart gunicorn" >>~/log
+sudo service gunicorn restart &&
+    log "restart gunicorn" || log "Fail: restart gunicorn"
 
-sudo service nginx restart
-echo -e "$(date +'%Y-%m-%d %R') restart nginx" >>~/log
+sudo service nginx restart &&
+    log "restart nginx" || log "Fail: restart nginx"
